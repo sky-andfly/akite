f9d36e1e0e80c9cb889a572e40db1877873fb32a
Sergey Golubiev <skyandfly@outlook.com>
2020-04-30 00:10:03 +0300

    front finaly

--- js/main.js
+++ js/main.js
@@ -1,52 +1,91 @@
-$(document).ready(
-    function () {
+$(document).ready(function(){
+    let files = [],
+        filesNames = [],
+        inputFile = $('input[type=file]'),
+        inputName = $('#name')
 
-        var form = $("#myform");
-        var message =  $('.form-status');
+    inputFile.on('change', function(){
+        files.push(this.files[0])
+        filesNames.push($(this).data('doc'))
+    });
 
+$('#send').on( 'click', function( event ){
+    event.stopPropagation()
+    event.preventDefault()
 
-        form.on('submit', function () {
-            var formData = new FormData();
-           
-
-            if (($('.input')[0].files).length != 0) {
-                $.each($('.input')[0].files, function(i, file){
-                    formData.append("file["+ i +"]", file);
-                });
-            }else{
-                message.html('Нужно выбрать файл');
-                return false;
+    $.each($('.important'), function(index, value){
+        var label = '.' + $(value).attr('id')
+        if($(value).val()){
+            $(label).removeClass('error')
+            $(label).addClass('success')
+        } else {
+            $(label).removeClass('success')
+            $(label).addClass('error')
+        }
+    })
+    if( files.length < 4 || inputName.val() == ''){
+        Swal.fire(
+            'Ошибка!',
+            'Заполните все необходимые поля.',
+            'error'
+          )
+        return
+    } else {
+        Swal.fire({
+            allowOutsideClick: false,
+            onOpen: () => {
+                Swal.showLoading()
             }
-            $.ajax({
-                type: "POST",
-                url: "php/ajax.php",
-                data: formData,
-                cache: false,
-                dataType: "json",
-                contentType: false,
-                processData: false,
-                beforeSend: function () { 
-                    console.log("Запрос начат");
-                    message.text("Запрос начат");
-                    form.find('input').prop("disabled", true);
-
-                 },
-                success: function (data) {
-                    if(data.status == 'ok'){
-                        message.text("Файл загружен");
-                        $('#myfile').val('');
-
-                    }else{
-                        message.text("Ошибка загрузки");
-                    }
-                },
-                complete: function() {
-                    console.log("Запрос окончен");
-                    form.find('input').prop("disabled", false);
-                },
-            });
-            return false;
-        });
-
+        })
     }
-);
+
+    let data = new FormData()
+
+    $.each( files, function( key, value ){
+        data.append( key, value )
+    });
+
+    data.append('user_info', inputName.val())
+    data.append('docNames', filesNames)
+    
+    $.ajax({
+        url: './php/ajax.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false,
+        contentType: false, 
+        success: function( respond, status, jqXHR ){
+            if(respond){
+                Swal.fire({
+                    title: 'Успешно!',
+                    text: 'Ваши фото были отправлены на рассмотрение.',
+                    icon: 'success'
+                  }).then((result) => {
+                      location.reload()
+                  })
+            }
+        },
+        error: function( jqXHR, status, errorThrown ){
+            console.log(errorThrown);
+            Swal.fire({
+                title: 'Ошибка!',
+                text: 'Попробуйте немного позже.',
+                icon: 'error'
+              })
+        }
+
+    });
+
+});
+
+let line = $('.header-shapes');
+$(document).on('mousemove', function(e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;  
+  line[0].style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)';
+  line[1].style.transform = 'translate(-' + x * 50 + 'px, +' + y * 50 + 'px)';
+})
+
+})
